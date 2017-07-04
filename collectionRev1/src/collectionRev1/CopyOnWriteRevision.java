95207c35f0dfebda24c0e702f625ee1c0a820b80
jarvis <napps.tech94@gmail.com>
2017-07-05 00:57:52 +0530

    change 2 by dev 2.

--- collectionRev1/src/collectionRev1/CopyOnWriteRevision.java
+++ collectionRev1/src/collectionRev1/CopyOnWriteRevision.java
@@ -12,6 +12,7 @@ public class CopyOnWriteRevision {
     // Test1
 	// changes by dev1.
 	// change by dev2.
+	// change by dev1.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CopyOnWriteArrayList<Object> list = new CopyOnWriteArrayList<>(Arrays.asList(new String[] { "a", "b" }));
